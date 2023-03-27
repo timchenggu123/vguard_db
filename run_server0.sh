eef7be1ef1b3d59aab8b1e9d05ea9789ae9b5b2d
Tim Gu <2013tim.g@gmail.com>
2023-03-27 16:51:59 -0400

    basic working version

---
+++ run_server0.sh
@@ -0,0 +1,1 @@
+python server_api.py --is_proposer true --id 0 --port 9860 --k_backups 2
--- server_api.py
+++ server_api.py
@@ -1,25 +1,35 @@
 from flask import Flask, jsonify, request, render_template
 from server_logic import Server
 import sqlite3
-import sys
 import argparse
-import requests
-import json
+import os
 
 app = Flask(__name__)
 logic=Server(app)
+app.config['address'] = {
+    0:('http://127.0.0.1', '9860'),
+    1:('http://127.0.0.1', '9861'),
+    2:('http://127.0.0.1', '9862'),
+    3:('http://127.0.0.1', '9863'),
+    4:('http://127.0.0.1', '9864'),
+    5:('http://127.0.0.1', '9865')
+}
 
 def init_db():
     dbname = f"database_{app.config['id']}.db"
-    connection = sqlite3.connect(dbname)
+    try:
+        connection = sqlite3.connect(dbname)
 
-    with open('schema.sql') as f:
-        connection.executescript(f.read())
-        
-    cur = connection.cursor()
+        with open('schema.sql') as f:
+            connection.executescript(f.read())
+            
+        cur = connection.cursor()
 
-    connection.commit()
-    connection.close()
+        connection.commit()
+        connection.close()
+    except:
+        os.remove(dbname)
+        init_db()
 
 def get_db_connection():
     dbname = f"database_{app.config['id']}.db"
@@ -27,6 +37,14 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+def insert_dummy_data():
+    conn=get_db_connection()
+    c = conn.cursor()
+    c.execute('INSERT INTO backup_data VALUES (?, ?, ?)', (None, 'abc', 'def'))
+    c.execute('INSERT INTO backup_data VALUES (?, ?, ?)', (None, 'ghi', 'jkl'))
+    conn.commit()
+    conn.close()
+    
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -46,12 +64,15 @@ def read_data():
 @app.route('/choose_backup', methods=['POST'])
 def choose_backup():
     conn = get_db_connection()
-    data = request.json['data']
+    data = request.json
     res = logic.on_chosen_as_backup(data, conn)
     conn.close()
-    return res
+    if res:
+        return '',200
+    else:
+        return '',201
 
-@app.route('/end_of_both', methods=['Get'])
+@app.route('/end_of_booth', methods=['Get'])
 def end_of_booth():
     conn = get_db_connection()
     if not app.config['is_proposer']:
@@ -59,15 +80,17 @@ def end_of_booth():
     data = request.json
     res = logic.on_end_of_booth(data, conn)
     conn.close()
+    return '', 200
     
 @app.route('/update_backup_list', methods=['POST'])
 def update_backup_list():
-    chosen_list = request.data['data']
+    chosen_list = request.json
     logic.update_backup_list(chosen_list)
+    return '',200
 
-@app.route('/delete_obsolete', method=['POST'])
+@app.route('/delete_obsolete', methods=['POST'])
 def delete_obsolete():
-    backup_list = request.data['data']
+    backup_list = request.json['data']
     conn = get_db_connection()
     logic.on_requested_delete_obsolete(backup_list=backup_list, conn=conn)
 
@@ -105,8 +128,8 @@ if __name__ == '__main__':
     app.config['id'] = args.id
     app.config['log_file'] = args.log_file
     app.config['k_backups'] = args.k_backups
-    app.config['addess'] = {
-        0:('https://127/0.0.1', '6161')
-    }
     app.config['port']=args.port
+    
+    init_db()
+    insert_dummy_data()
     app.run(debug=True,port=args.port)
