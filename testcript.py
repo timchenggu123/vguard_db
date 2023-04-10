b669a0a831e59ca03c96509720eb77813a7c4c58
Tim Gu <2013tim.g@gmail.com>
2023-04-10 16:51:00 -0400

    update test script

--- testcript.py
+++ testcript.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from time import sleep
 
 proposer = 'http://127.0.0.1:9860'
 
@@ -8,12 +9,21 @@ diffs = []
 while True:
     res = requests.get(f'{proposer}/read_latest')
     code = res.status_code
-    if code != 200:
-        print(f"Proposer not responding. Test terminated. average latency: {sum(diffs)/len(diffs)}")
-    ts = res.json()
-    ts = float(ts)
-    if ts > latest:
-        latest = ts
-        now = time.time() * 1e6
-        diffs.append(now - ts)
-        print(diffs[-1])
+    if code == 200:
+        print(f"Proposer not responding. Waiting...")
+        break
+    
+for i in range(10):
+    res = requests.get(f'{proposer}/read_latest')
+    while True:
+        sleep(0.1)
+        ts = res.json()
+        ts = float(ts)
+        if ts > latest:
+            latest = ts
+            now = time.time() * 1e6
+            diffs.append(now - ts)
+            print(diffs[-1])
+            break
+        
+print(f'Test finished. Average latency is {sum(diffs)/len(diffs) * 1e6} seconds')
